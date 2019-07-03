652b36229a1ea3bfde12428357980e8e71e63080
rhoffenexitquitgit config --global user.name rhoffen <rebecca.hoffenberg@gmail.com>
2019-07-03 14:45:27 -0700

    Logic with two questions added.

--- index.js
+++ index.js
@@ -2,17 +2,30 @@ const guesses = document.querySelectorAll("li");
 
 guesses.forEach(guess => addEventListener("click", handleClick));
 
+const responses = {
+    "q1": {
+        incorrect: "Incorrect.  Purple Rain was the best album of 1983!",
+        buttonId: "result1"
+    },
+    "q2": {
+        incorrect: "Incorrect.  Darryl McDaniel is the DMC of Run-DMC.",
+        buttonId: "result2"
+    }
+}
+
 function handleClick(event) {
     const answer = event.target.classList[0];
-    console.log(answer);
-    const result = document.getElementById('result1');
+    const questionId = event.target.closest("ol").previousElementSibling.id;
+    const resultBox = document.getElementById(responses[questionId].buttonId);
+
     if (answer === 'correct') {
-        result1.style.display = "block";
-        result1.style.color = "purple";
-        result1.innerHTML = "Correct!  Purple Rain was the best album of 1983!";
+        resultBox.style.display = "block";
+        resultBox.style.color = "purple";
+        resultBox.innerHTML = "Correct!";
     } else {
-        result1.style.display = "block";
-        result1.style.color = "red";
-        result1.innerHTML = "Incorrect.  Purple Rain was the best album of 1983!";
+        resultBox.style.display = "block";
+        resultBox.style.color = "red";
+        resultBox.innerHTML = responses[questionId].incorrect;
     }
+    setTimeout(function(){resultBox.style.display = "none"}, 3000);
 }
