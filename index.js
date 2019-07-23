8a4916d9b05381418719c0da9cdd8b2ebfeb3569
rhoffenexitquitgit config --global user.name rhoffen <rebecca.hoffenberg@gmail.com>
2019-07-23 15:53:05 -0700

    q1 data abstracted into object file.  Next: q2.

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+const q1Data = questionData.q1;
+
+const q1 = document.getElementById("q1");
+const q1o1 = document.getElementById("q1o1");
+const q1o2 = document.getElementById("q1o2");
+const q1o3 = document.getElementById("q1o3");
+
+const optionListq1 = [q1o1, q1o2, q1o3];
+
+q1.innerHTML = '1) '  + q1Data.question;
+q1o1.innerHTML = 'a. '  + q1Data.option1[0];
+q1o2.innerHTML = 'b. '  + q1Data.option2[0];
+q1o3.innerHTML = 'c. '  + q1Data.option3[0];
+
+for (let index = 1; index <= optionListq1.length; index++) {
+    let element = optionListq1[index-1];
+    let optionKey = 'option' + index;
+    let option = q1Data[optionKey];
+    
+    if (option[1]) {
+        console.log(option);
+        element.classList.add("correct");
+        console.log(element.classList);
+    }
+}
 //Add event listener for hint buttons
 const hints = document.querySelectorAll(".hint");
 //console.log(hints);
@@ -9,10 +34,10 @@ guesses.forEach(guess => addEventListener("click", handleClick));
 
 //Response object to hold properties for different questions. Eventually, all the questions and answers will be stored in database fields.
 const responses = {
-    "q1": {
-        incorrect: "Incorrect.  Purple Rain was the best album of 1983!",
-        buttonId: "result1"
-    },
+    // "q1": {
+    //     incorrect: "Incorrect.  Purple Rain was the best album of 1983!",
+    //     buttonId: "result1"
+    // },
     "q2": {
         incorrect: "Incorrect.  Darryl McDaniels is the DMC of Run-DMC.",
         buttonId: "result2"
@@ -22,7 +47,6 @@ const responses = {
 //Callback function to handle hint requests
 function handleHint(event) {
     const id = event.target.id;
-    console.log(id);
     if (id === "hint1") {
         document.body.style.backgroundColor = "purple";
         event.target.style.fontSize = "30px";
@@ -46,9 +70,13 @@ function handleHint(event) {
 
 //Callback function to handle click events on list items.
 function handleClick(event) {
+    console.log(event.target);
+    
     const answer = event.target.classList[0]; //Checks for class of 'Correct' on clicked element
-    const questionId = event.target.closest("ol").previousElementSibling.id; //Gets the id for the question associated with the list item by traversing the DOM up to the <h2> that contains the question ID.
-    const resultBox = document.getElementById(responses[questionId].buttonId);  //Gets the button element associated with that question where the result will appear.
+    console.log(answer);
+    
+    const questionId = event.target.closest("ul").previousElementSibling.id; //Gets the id for the question associated with the list item by traversing the DOM up to the <h2> that contains the question ID.
+    const resultBox = document.getElementById(questionData[questionId].resultBoxId);  //Gets the button element associated with that question where the result will appear.  
 
     if (answer === 'correct') { // If the <li> has a class of "correct"
         resultBox.style.display = "block"; //Change display to "block" (from 'none' in the CSS file)
@@ -57,7 +85,7 @@ function handleClick(event) {
     } else {
         resultBox.style.display = "block";  //Change display to "block" (from 'none' in the CSS file)
         resultBox.style.color = "red";  //Inorrect answer button text printed in red
-        resultBox.innerHTML = responses[questionId].incorrect; //Incorrect answer text varies with question
+        resultBox.innerHTML = questionData[questionId].incorrectText; //Incorrect answer text varies with question
     }
     setTimeout(function(){resultBox.style.display = "none"}, 3000); //Result button disappears in 3s
 }
---
+++ quiz-data.js
@@ -0,0 +1,11 @@
+const questionData = {};
+
+questionData["q1"] = {
+    question: "What was the best album from 1983?",
+    "option1": ["Purple Rain", true],
+    "option2": ["Thriller", false],
+    "option3": ["Like a Virgin", false],
+    incorrectText: "Incorrect.  Purple Rain was the best album of 1983.",
+    hintText: "Do a background check!",
+    resultBoxId: "result1"
+}
